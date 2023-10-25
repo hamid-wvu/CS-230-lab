37038dd2c4910bbd1da0d139aa42b6b50934f0c3
hamid.wvu <an00037@mix.wvu.edu>
2023-10-24 20:16:19 -0400

    nav and card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Cards';
 
 function App() {
   return (
     <div className="App">
-   <h1>CS 230</h1>
-   <h2>Section-02</h2>
-   <p>WVU ID: 800406312</p>
-   <p>Hi I am Hamid</p>
+      <Navbar />
+     
+      <h1>CS 230</h1>
+      <h2>Section-02</h2>
+      <p>WVU ID: 800406312</p>
+      <p>Hi I am Hamid</p>
+      <Card />
+
     </div>
   );
 }
